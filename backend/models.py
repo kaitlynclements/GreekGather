65af678c3f84aab68f5706cca596cdf9c245c16b
Elizabeth (Lizzie) Soltis <soltis.el@gmail.com>
2025-02-10 12:02:31 -0600

    Add files via upload

--- backend/models.py
+++ backend/models.py
@@ -24,21 +24,20 @@ class Chapter(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     role = db.Column(db.String(50), default="member")  # Roles: member, vp, admin
     chapter_id = db.Column(db.Integer, db.ForeignKey("chapter.id"))
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.name}>'
 
 
 class Event(db.Model):
