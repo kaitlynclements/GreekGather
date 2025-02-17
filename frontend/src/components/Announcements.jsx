1e6e8806ab091c3f6437ea7290a36dbba0ea7a03
sammuehlebach <118694812+sammuehlebach@users.noreply.github.com>
2025-02-16 19:46:30 -0600

    Update Announcements.jsx

--- frontend/src/components/Announcements.jsx
+++ frontend/src/components/Announcements.jsx
@@ -1,3 +1,22 @@
+/*
+ * Name: Announcements.jsx
+ * Description: Component for displaying announcements in GreekGather.
+ * Programmer: Kaitlyn Clements, Taylor Slade, Lizzie Soltis, Aaditi Chinawalkar, Sam Muehlebach
+ * Created: February 2nd, 2025
+ * Revised: Refer to Github Commits
+ * Revisions: [Revision Date]: [Description of change] (Author)
+ * Preconditions: Announcements data must be available via the backend.
+ * Acceptable Inputs: None (fetches announcements).
+ * Unacceptable Inputs: None.
+ * Postconditions: Displays a list of announcements.
+ * Return Values: React JSX component.
+ * Errors & Exceptions: API fetches errors if announcements can't be retrieved.
+ * Side Effects: UI updates dynamically.
+ * Invariants: Announcements must be kept up-to-date.
+ * Known Faults: N/A.
+ */
+
+
 import React from 'react';
 import './Announcements.css';
 
