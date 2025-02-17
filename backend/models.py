c10ff1148b94feef55889e140b3498fbe69034ab
kaitlynclements <123575277+kaitlynclements@users.noreply.github.com>
2025-02-16 19:41:09 -0600

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -27,7 +27,7 @@ class User(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
-    role = db.Column(db.String(50), default="member")  # Roles: member, vp, admin
+    role = db.Column(db.String(50), default="member")  # Roles: member, exec, admin
     chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=True)  
 
     def set_password(self, password):
