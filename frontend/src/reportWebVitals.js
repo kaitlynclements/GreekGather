b5741f5219b8ef787eab207686ec05489e285176
AaditiC <123911761+AaditiC@users.noreply.github.com>
2025-02-16 19:58:06 -0600

    added header descriptions

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,3 +1,20 @@
+"""
+ * Name: reportWebVitals.js
+ * Description: This file will measure and report website metrics 
+ * Programmer: Kaitlyn Clements, Taylor Slade, Lizzie Soltis, Aaditi Chinawalkar, Sam Muehlebach
+ * Created: February 2nd, 2025
+ * Revised: Refer to Github commits.
+ * Revisions: Refer to Github commits.
+ * Preconditions: Flask and dependencies must be installed.
+ * Acceptable Inputs: HTTP requests to intended endpoints.
+ * Unacceptable Inputs: Incorrect requests.
+ * Postconditions: Routes are registered and server starts.
+ * Return Values: N/A
+ * Errors & Exceptions: Raises errors if database is not connected.
+ * Side Effects: Flask app initialized
+ * Invariants: Server must remain available.
+ * Known Faults: N/A
+"""
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
