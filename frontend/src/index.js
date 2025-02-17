c7224542eae5fdcff7d4479573adfc86ef48d82d
AaditiC <123911761+AaditiC@users.noreply.github.com>
2025-02-16 19:56:10 -0600

    added header description

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,20 @@
+"""
+ * Name: index.js
+ * Description: This file is the entry point for React 
+ * Programmer: Kaitlyn Clements, Taylor Slade, Lizzie Soltis, Aaditi Chinawalkar, Sam Muehlebach
+ * Created: February 2nd, 2025
+ * Revised: Refer to Github commits.
+ * Revisions: Refer to Github commits.
+ * Preconditions: Flask and dependencies must be installed.
+ * Acceptable Inputs: HTTP requests to intended endpoints.
+ * Unacceptable Inputs: Incorrect requests.
+ * Postconditions: Routes are registered and server starts.
+ * Return Values: N/A
+ * Errors & Exceptions: Raises errors if database is not connected.
+ * Side Effects: Flask app initialized
+ * Invariants: Server must remain available.
+ * Known Faults: N/A
+"""
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
