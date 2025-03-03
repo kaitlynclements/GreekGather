fd73687527d8e56aaa2b20b47a588a1db48868af
Elizabeth (Lizzie) Soltis <soltis.el@gmail.com>
2025-03-02 20:30:18 -0600

    Update ManageEvents.jsx
    adjusted to handle chapter_id data from user

--- frontend/src/components/ManageEvents.jsx
+++ frontend/src/components/ManageEvents.jsx
@@ -24,7 +24,7 @@ function ManageEvents() {
     const [events, setEvents] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [newEvent, setNewEvent] = useState({
-        id: null,
+        id: '',
         name: '',
         description: '',
         date: '',
